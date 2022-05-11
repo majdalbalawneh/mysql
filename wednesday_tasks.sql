d082f87acbb721b5f77a9eb984cd4a4d33385f3f
majdalbalawneh <majd3180157@gmail.com>
2022-05-11 20:43:26 +0300

    2task

---
+++ wednesday_tasks.sql
@@ -0,0 +1,213 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 11, 2022 at 07:41 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wednesday_tasks`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `country_new`
+--
+
+CREATE TABLE `country_new` (
+  `country_id` varchar(50) NOT NULL,
+  `country_name` varchar(50) NOT NULL,
+  `region_id` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `country_new`
+--
+
+INSERT INTO `country_new` (`country_id`, `country_name`, `region_id`) VALUES
+('AR', 'Argentina', '2'),
+('AU', 'Austuralia', '3'),
+('BE', 'Belgium', '1'),
+('BR', 'Brazil', '2'),
+('CA', 'Canada', '2'),
+('CH', 'Switzerland', '1'),
+('CN', 'China', '3'),
+('DE', 'Germany', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departments`
+--
+
+CREATE TABLE `departments` (
+  `department_id` decimal(4,0) NOT NULL,
+  `department_name` varchar(30) NOT NULL,
+  `manager_id` decimal(6,0) NOT NULL,
+  `location_id` decimal(4,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `departments`
+--
+
+INSERT INTO `departments` (`department_id`, `department_name`, `manager_id`, `location_id`) VALUES
+('10', 'Administration', '200', '1700'),
+('20', 'Marketing', '201', '1800'),
+('30', 'Purchasing', '114', '1700'),
+('40', 'Human Resources', '203', '2400'),
+('50', 'Shipping', '121', '1500'),
+('60', 'IT', '103', '1400'),
+('70', 'Public Relations', '204', '2700'),
+('80', 'Sales', '145', '2500'),
+('90', 'Executive', '100', '1700'),
+('100', 'Finance', '108', '1700'),
+('110', 'Accounting', '205', '1700'),
+('120', 'Treasury', '0', '1700'),
+('130', 'Corporate Tax', '0', '1700'),
+('140', 'Control And Credit', '0', '1700'),
+('150', 'Shareholder Services', '0', '1700'),
+('160', 'Benefits', '0', '1700'),
+('170', 'Manufacturing', '0', '1700'),
+('180', 'Construction', '0', '1700'),
+('190', 'Contracting', '0', '1700'),
+('200', 'Operations', '0', '1700'),
+('210', 'IT Support', '0', '1700'),
+('220', 'NOC', '0', '1700'),
+('230', 'IT Helpdesk', '0', '1700'),
+('240', 'Government Sales', '0', '1700'),
+('250', 'Retail Sales', '0', '1700'),
+('260', 'Recruiting', '0', '1700'),
+('270', 'Payroll', '0', '1700');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `employee_id` decimal(6,0) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `phone_number` varchar(20) NOT NULL,
+  `hire_date` date NOT NULL,
+  `job_id` varchar(10) NOT NULL,
+  `salary` decimal(8,2) NOT NULL,
+  `commission_pct` decimal(2,2) NOT NULL,
+  `manager_id` decimal(6,0) NOT NULL,
+  `department_id` decimal(4,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`employee_id`, `first_name`, `last_name`, `email`, `phone_number`, `hire_date`, `job_id`, `salary`, `commission_pct`, `manager_id`, `department_id`) VALUES
+('100', 'Steven', 'King', 'SKING', '515.123.4567', '1987-06-17', 'AD_PRES', '24000.00', '0.00', '0', '90'),
+('101', 'Neena', 'Kochhar', 'NKOCHHAR', '515.123.4568', '1987-06-18', 'AD_VP', '17000.00', '0.00', '100', '90'),
+('102', 'Lex', 'De Haan', 'LDEHAAN', '515.123.4569', '1987-06-19', 'AD_VP', '17000.00', '0.00', '100', '90'),
+('103', 'Alexander', 'Hunold', 'AHUNOLD', '590.423.4567', '1987-06-20', 'IT_PROG', '9000.00', '0.00', '102', '60'),
+('104', 'Bruce', 'Ernst', 'BERNST', '590.423.4568', '1987-06-21', 'IT_PROG', '6000.00', '0.00', '103', '60'),
+('105', 'David', 'Austin', 'DAUSTIN', '590.423.4569', '1987-06-22', 'IT_PROG', '4800.00', '0.00', '103', '60'),
+('106', 'Valli', 'Pataballa', 'VPATABAL', '590.423.4560', '1987-06-23', 'IT_PROG', '4800.00', '0.00', '103', '60'),
+('107', 'Diana', 'Lorentz', 'DLORENTZ', '590.423.5567', '1987-06-24', 'IT_PROG', '4200.00', '0.00', '103', '60'),
+('108', 'Nancy', 'Greenberg', 'NGREENBE', '515.124.4569', '1987-06-25', 'FI_MGR', '12000.00', '0.00', '101', '100'),
+('109', 'Daniel', 'Faviet', 'DFAVIET', '515.124.4169', '1987-06-26', 'FI_ACCOUNT', '9000.00', '0.00', '108', '100'),
+('110', 'John', 'Chen', 'JCHEN', '515.124.4269', '1987-06-27', 'FI_ACCOUNT', '8200.00', '0.00', '108', '100'),
+('111', 'Ismael', 'Sciarra', 'ISCIARRA', '515.124.4369', '1987-06-28', 'FI_ACCOUNT', '7700.00', '0.00', '108', '100'),
+('112', 'Jose Manuel', 'Urman', 'JMURMAN', '515.124.4469', '1987-06-29', 'FI_ACCOUNT', '7800.00', '0.00', '108', '100'),
+('113', 'Luis', 'Popp', 'LPOPP', '515.124.4567', '1987-06-30', 'FI_ACCOUNT', '6900.00', '0.00', '108', '100'),
+('114', 'Den', 'Raphaely', 'DRAPHEAL', '515.127.4561', '1987-07-01', 'PU_MAN', '11000.00', '0.00', '100', '30'),
+('115', 'Alexander', 'Khoo', 'AKHOO', '515.127.4562', '1987-07-02', 'PU_CLERK', '3100.00', '0.00', '114', '30'),
+('116', 'Shelli', 'Baida', 'SBAIDA', '515.127.4563', '1987-07-03', 'PU_CLERK', '2900.00', '0.00', '114', '30'),
+('117', 'Sigal', 'Tobias', 'STOBIAS', '515.127.4564', '1987-07-04', 'PU_CLERK', '2800.00', '0.00', '114', '30'),
+('118', 'Guy', 'Himuro', 'GHIMURO', '515.127.4565', '1987-07-05', 'PU_CLERK', '2600.00', '0.00', '114', '30'),
+('119', 'Karen', 'Colmenares', 'KCOLMENA', '515.127.4566', '1987-07-06', 'PU_CLERK', '2500.00', '0.00', '114', '30'),
+('120', 'Matthew', 'Weiss', 'MWEISS', '650.123.1234', '1987-07-07', 'ST_MAN', '8000.00', '0.00', '100', '50'),
+('121', 'Adam', 'Fripp', 'AFRIPP', '650.123.2234', '1987-07-08', 'ST_MAN', '8200.00', '0.00', '100', '50'),
+('122', 'Payam', 'Kaufling', 'PKAUFLIN', '650.123.3234', '1987-07-09', 'ST_MAN', '7900.00', '0.00', '100', '50'),
+('123', 'Shanta', 'Vollman', 'SVOLLMAN', '650.123.4234', '1987-07-10', 'ST_MAN', '6500.00', '0.00', '100', '50'),
+('124', 'Kevin', 'Mourgos', 'KMOURGOS', '650.123.5234', '1987-07-11', 'ST_MAN', '5800.00', '0.00', '100', '50'),
+('125', 'Julia', 'Nayer', 'JNAYER', '650.124.1214', '1987-07-12', 'ST_CLERK', '3200.00', '0.00', '120', '50'),
+('126', 'Irene', 'Mikkilineni', 'IMIKKILI', '650.124.1224', '1987-07-13', 'ST_CLERK', '2700.00', '0.00', '120', '50'),
+('127', 'James', 'Landry', 'JLANDRY', '650.124.1334', '1987-07-14', 'ST_CLERK', '2400.00', '0.00', '120', '50'),
+('203', 'Susan', 'Mavris', 'SMAVRIS', '515.123.7777', '1987-09-28', 'HR_REP', '6500.00', '0.00', '101', '40');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locations`
+--
+
+CREATE TABLE `locations` (
+  `locations_id` int(11) NOT NULL,
+  `street_address` varchar(50) NOT NULL,
+  `postal_code` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `state_province` varchar(50) NOT NULL,
+  `region_id` varchar(20) NOT NULL,
+  `country_id` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `locations`
+--
+
+INSERT INTO `locations` (`locations_id`, `street_address`, `postal_code`, `city`, `state_province`, `region_id`, `country_id`) VALUES
+(1000, '1297 Via Cola di Rie', '989', 'Roma', '', '', 'IT'),
+(1100, '93091 Calle della Testa', '10934', 'Venice', '', '', 'IT'),
+(1200, '2017 Shinjuku-ku', '1689', 'Tokyo', 'Tokyo Prefecture', '', 'JP'),
+(1300, '9450 Kamiya-cho', '6823', 'Hiroshima', '', '', 'JP'),
+(1400, '2014 Jabberwocky Rd', '26192', 'Southlake', 'Texas', '', 'US'),
+(1500, '2011 Interiors Blvd', '99236', 'South San Francisco', 'California', '', 'US'),
+(1600, '2007 Zagora St', '50090', 'South Brunswick', 'New Jersey', '', 'US'),
+(1700, '2004 Charade Rd', '98199', 'Seattle', 'Washington', '', 'US'),
+(1800, '147 Spadina Ave', 'M5V 2L7', 'Toronto', 'Ontario', '', 'CA');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `country_new`
+--
+ALTER TABLE `country_new`
+  ADD PRIMARY KEY (`country_id`);
+
+--
+-- Indexes for table `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`department_id`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`employee_id`);
+
+--
+-- Indexes for table `locations`
+--
+ALTER TABLE `locations`
+  ADD PRIMARY KEY (`locations_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
